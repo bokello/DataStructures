6235af15768e1b03ce60f1243a3185e7efd6eeb3
B.OKELLO <bokello2010@gmail.com>
2022-04-14 07:54:23 +0300

    Time: 113 ms (83.69%), Space: 48.5 MB (8.23%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        
+        if(root == null || val == root.val)
+            return root;
+                        
+        return val < root.val ? SearchBST(root.left,val) : SearchBST(root.right,val);
+        
+    }
+}
