a34dd81c76ba20ae196662ce28608da4ffa49dd3
B.OKELLO <bokello2010@gmail.com>
2022-05-12 06:30:05 +0300

    Time: 1530 ms (11.04%), Space: 41.1 MB (38.10%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public string RemoveDuplicates(string s) {
+           
+           string result = string.Empty;
+           Stack<char> stack = new Stack<char>();
+
+           for(int i= s.Length - 1;i >=0;i--)
+           {
+               if(stack.Count == 0 || stack.Peek() != s[i])
+                   stack.Push(s[i]);
+               else
+                   stack.Pop();
+           }
+        
+           while(stack.Count != 0)
+               result += stack.Pop();
+        
+          return result;
+          
+    }
+}
