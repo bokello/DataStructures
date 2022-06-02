b52e44f796e0efd4a53ee68b59a2ca8d5f6c4601
B.OKELLO <bokello2010@gmail.com>
2022-06-02 06:53:21 +0300

    Time: 115 ms (78.28%), Space: 43.9 MB (8.54%) - LeetHub

--- 268-missing-number/268-missing-number.cs
+++ 268-missing-number/268-missing-number.cs
@@ -1,7 +1,21 @@
 public class Solution {
     public int MissingNumber(int[] nums) {
         
-        int n = nums.Length;
+        
+        int x = nums.Length;
+        
+        
+        for(int i=0;i < nums.Length; i++)
+        {
+            
+            x ^= i ^ nums[i];
+            
+        }
+        
+        return x;
+            
+        
+       /* int n = nums.Length;
         int totalSum = 0;
         
         int expectedSum = n * (n + 1) / 2;
@@ -10,7 +24,7 @@ public class Solution {
           totalSum += i;
         
         return expectedSum - totalSum;
-        
+        */
         
     }
     
